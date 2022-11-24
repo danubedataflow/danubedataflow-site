40de82004246be213d00911507066ff26236d737
Marcel Gruenauer <bigghosthead@gmail.com>
2022-11-24 12:50:19 +0100

    added experiment E02 (sin curves in x/y producing the infinity sign)

---
+++ src/experiments/E02/sketch.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const config = new Config()
+    .title('E02')
+    .maxIterations(1);
+
+makeForm(
+    makeSlider('xFactor', 'Horizontal factor', 1, 5, 2.5, 0.1),
+    makeSlider('alpha', 'Alpha', 1, 255, 10),
+    makeSlider('angleStep', 'Angle Step', 0.1, 10, 0.5, 0.1),
+    makeSlider('squareSize', 'Square size', 1, 100, [30, 50]),
+);
+
+function initSketch() {
+    stroke(0, ctrl.alpha);
+    strokeWeight(1);
+    noFill();
+    rectMode(CENTER);
+}
+
+function drawSketch() {
+    background(255);
+    translate(width / 2, height / 2);
+    for (let i = 0; i < 360; i += ctrl.angleStep) {
+
+        // x-factor 2 produces the "infinity sign"
+
+        let x = sin(i * ctrl.xFactor) * (width / 3);
+        let y = sin(i) * (height / 3);
+
+        // Draw random squares around each point, drawn at 10% alpha, produces
+        // a fuzzy shape.
+
+        let dim = random(...ctrl.squareSize);
+        rect(x, y, dim, dim);
+    }
+}
