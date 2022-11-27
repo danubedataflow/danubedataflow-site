0dbd69c94a23f7413d820f8b83192a6baf6dbcb9
Marcel Gruenauer <bigghosthead@gmail.com>
2022-11-27 10:31:46 +0100

    numbers.js: added Array.prototype.permutations()

--- src/js/numbers.js
+++ src/js/numbers.js
@@ -47,3 +47,22 @@ Array.prototype.shuffle = function() {
     return this;
 };
 
+// https://stackoverflow.com/a/20871714
+Array.prototype.permutations = function() {
+    let result = [];
+
+    const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+            result.push(m)
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr.slice();
+                let next = curr.splice(i, 1);
+                permute(curr.slice(), m.concat(next))
+            }
+        }
+    }
+
+    permute(this);
+    return result;
+}
