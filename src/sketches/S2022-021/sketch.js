295e9ad422eed9a3f75a5fefa7b336144e9b75e5
Marcel Gruenauer <bigghosthead@gmail.com>
2022-11-28 10:23:36 +0100

    S2022-021: extracted generic controlColorWithAlpha() function

--- src/sketches/S2022-021/sketch.js
+++ src/sketches/S2022-021/sketch.js
@@ -17,6 +17,16 @@ function makeSelectColor() {
     return containerDiv;
 }
 
+/* Generic function to create a color based on two controls: one for the color
+ * (default name 'color') and one range for the alpha (default name 'alpha').
+ */
+
+function controlColorWithAlpha(colorControl = 'color', alphaControl = 'alpha') {
+    let c = color(ctrl[colorControl]);
+    c.setAlpha(int(random(...ctrl[alphaControl])));
+    return c;
+}
+
 makeForm(
     makeSlider('horizontalBars', 'Number of horizontal bars', 1, 10, 5),
     makeSlider('verticalBars', 'Number of vertical bars', 1, 10, 5),
@@ -38,17 +48,13 @@ function drawSketch() {
     for (let i = 0; i <= ctrl.horizontalBars; i++) {
         let x1 = int(random(width));
         let w = int(random(...ctrl.size.map(x => x * width / 100)));
-        let c = color(ctrl.color);
-        c.setAlpha(int(random(...ctrl.alpha)));
-        fill(c);
+        fill(controlColorWithAlpha());
         rect(x1, 0, w, height);
     }
     for (let i = 0; i <= ctrl.verticalBars; i++) {
         let y1 = int(random(width));
         let h = int(random(...ctrl.size.map(x => x * height / 100)));
-        let c = color(ctrl.color);
-        c.setAlpha(int(random(...ctrl.alpha)));
-        fill(c);
+        fill(controlColorWithAlpha());
         rect(0, y1, width, h);
     }
 }
