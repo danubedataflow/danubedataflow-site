42faaac8bf348555a0f84c53cafdc5454ca76710
Marcel Gruenauer <bigghosthead@gmail.com>
2022-12-07 15:28:01 +0100

    S2022-025 now has two color strategies: 'random' and 'adjacent'

--- src/sketches/S2022-025/sketch.js
+++ src/sketches/S2022-025/sketch.js
@@ -7,9 +7,13 @@ const config = new Config()
 makeForm(
     makeSlider('numTiles', 'Number of tiles', 4, 40, 20),
     makeSlider('chanceDiagonal', 'Chance for orientation of the diagonal (%)', 0, 100, 50),
+    makeSelect('colorStrategy', 'Color selection strategy',
+        makeOption('random', 'Random'),
+        makeOption('adjacent', 'Adjacent'),
+    ),
 );
 
-let palette;
+let palette, c1, c2;
 
 function initSketch() {
     noStroke();
@@ -25,19 +29,12 @@ function drawSketch() {
             let {
                 dim
             } = config;
-            let p = [...palette]; // clone
             let ul = [-dim / 2, -dim / 2];
             let ur = [dim / 2, -dim / 2];
             let ll = [-dim / 2, dim / 2];
             let lr = [dim / 2, dim / 2];
 
-            // choose two different random colors
-            let c1 = random(p);
-            let c2;
-            do {
-                c2 = random(p);
-            } while (c1 == c2);
-
+            chooseColors();
             if (random(100) < ctrl.chanceDiagonal) {
                 // upper left to lower right
                 fill(c1);
@@ -48,10 +45,32 @@ function drawSketch() {
             } else {
                 // upper right to lower left
                 fill(c1);
-                triangle(...ur, ...ll, ...lr);
-                fill(c2);
                 triangle(...ur, ...ll, ...ul);
+                fill(c2);
+                triangle(...ur, ...ll, ...lr);
             }
+
+            c1 = c2;
         }
     });
+}
+
+function chooseColors() {
+    if (ctrl.colorStrategy === 'random') {
+        // choose two different random colors
+
+        c1 = random(palette);
+        do {
+            c2 = random(palette);
+        } while (c1 == c2);
+
+    } else if (ctrl.colorStrategy === 'adjacent') {
+        c1 = c2; // reuse previous color
+        if (c1 === undefined) c1 = random(palette);
+        do {
+            c2 = random(palette);
+        } while (c1 == c2);
+    } else {
+        console.log('invalid color strategy ' + ctrl.colorStrategy);
+    }
 }
