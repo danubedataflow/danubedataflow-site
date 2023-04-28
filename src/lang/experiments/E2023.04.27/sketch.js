360e47b23e981fa6968ab6b10a3e3db797876077
Marcel Gruenauer <bigghosthead@gmail.com>
2023-04-28 12:21:05 +0200

    work on E2023.04.27

--- src/lang/experiments/E2023.04.27/sketch.js
+++ src/lang/experiments/E2023.04.27/sketch.js
@@ -3,17 +3,86 @@
 function setup() {
     createCanvas(...getCanvasDimension()).parent('sketch');
     makeForm(
-        makeSlider('greyValue', '[% t.sl('Grey value', 'Grauwert', '灰色値') %]', 0, 255, 128),
+        makeSelectColorMap(),
+        makeSlider('numColors', '[% t.numColors %]', 2, 32, 16),
+        makeSlider('chanceColor', 'Chance of any color (%)', 0, 100, 50),
+        makeSlider('numTiles', '[% t.numTiles %]', 4, 40, 10),
     );
-    stroke('black');
+    noStroke();
     rectMode(CORNERS);
     noLoop();
 }
 
 function draw() {
     readControls();
-    fill(ctrl.greyValue);
-    rect(0, 0, width, height);
+
+    background("white");
+    let palette = chroma.scale(ctrl.colorMap).colors(ctrl.numColors);
+
+    let dim = width / ctrl.numTiles;
+    for (let y = 1; y <= ctrl.numTiles; y++) {
+        for (let x = 1; x <= ctrl.numTiles; x++) {
+            push();
+            translate((x - 1) * dim, (y - 1) * dim);
+
+            let ul = [0, 0];
+            let ur = [dim, 0];
+            let ll = [0, dim];
+            let lr = [dim, dim];
+
+            // upper mid, right mid, bottom mid, left mid
+            let um = [dim / 2, 0];
+            let rm = [dim, dim / 2];
+            let bm = [dim / 2, dim];
+            let lm = [0, dim / 2];
+            let center = [dim / 2, dim / 2];
+
+            let c;
+
+            if (random(100) < ctrl.chanceColor) {
+                c = random(palette);
+                fill(c);
+            } else {
+                c = "white"
+            }
+            rect(...ul, ...lr);
+
+            if (random(100) < ctrl.chanceColor) {
+                c = random(palette);
+                fill(c);
+            } else {
+                c = "white"
+            }
+            triangle(...um, ...center, ...lm);
+
+
+            if (random(100) < ctrl.chanceColor) {
+                c = random(palette);
+                fill(c);
+            } else {
+                c = "white"
+            }
+            triangle(...um, ...center, ...rm);
+
+            if (random(100) < ctrl.chanceColor) {
+                c = random(palette);
+                fill(c);
+            } else {
+                c = "white"
+            }
+            triangle(...bm, ...center, ...lm);
+
+            if (random(100) < ctrl.chanceColor) {
+                c = random(palette);
+                fill(c);
+            } else {
+                c = "white"
+            }
+            triangle(...bm, ...center, ...rm);
+
+            pop();
+        }
+    }
 }
 
 function windowResized() {
