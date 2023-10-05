3968bf03bf96d1ec21eca45aa6181a984857fcac
Marcel Gruenauer <bigghosthead@gmail.com>
2023-10-05 11:45:37 +0200

    W2022.11.19: Flexiblere Farb-Parameter; HSV-Farbmodell

--- src/W2022.11.19/sketch.js
+++ src/W2022.11.19/sketch.js
@@ -1,33 +1,25 @@
 'use strict';
 
-/* Generic function to create a color based on two controls: one for the color
- * (default name 'color') and one range for the alpha (default name 'alpha').
- */
-
-function controlColorWithAlpha(colorControl = 'color', alphaControl = 'alpha') {
-    let c = color(ctrl[colorControl]);
-    c.setAlpha(int(random(...ctrl[alphaControl])));
-    return c;
-}
-
 function setupForm() {
     makeForm(
         makeSlider('horizontalBars', 'Anzahl der horizontalen Striche', 1, 10, 5),
         makeSlider('verticalBars', 'Anzahl der vertikalen Striche', 1, 10, 5),
         makeSlider('size', 'Strichdicke als Prozentsatz der Leinwandgröße', 1, 20, [9, 13]),
         makeFieldset('Farben',
-            makeSelect(
-                'color',
-                'Farbe', [
-                    makeOption('black', 'Schwarz'),
-                    makeOption('red', 'Rot'),
-                    makeOption('green', 'Grün'),
-                    makeOption('blue', 'Blau'),
-                ],
-                'black'
-            ),
+            makeSlider('colorAngle', 'Winkel auf dem Farbenkreis', 0, 359, 0),
+            makeSlider('saturation', 'Farbsättigung', 40, 100, [60, 80]),
+            makeSlider('brightness', 'Helligkeit', 40, 100, [60, 80]),
             makeSlider('alpha', 'Transparenz (Alpha)', 1, 255, [50, 200]),
         ),
+    );
+}
+
+function setRandomFillColor() {
+    fill(
+        ctrl.colorAngle,
+        randomIntRange(...ctrl.saturation),
+        randomIntRange(...ctrl.brightness),
+        randomIntRange(...ctrl.alpha)
     );
 }
 
@@ -36,17 +28,18 @@ function drawSketch() {
     noStroke();
     fill('black');
     rectMode(CORNER);
+    colorMode(HSB, 360, 100, 100, 100);
 
     for (let i = 0; i <= ctrl.horizontalBars; i++) {
         let x1 = int(random(width));
         let w = int(random(...ctrl.size.map(x => x * width / 100)));
-        fill(controlColorWithAlpha());
+        setRandomFillColor();
         rect(x1, 0, w, height);
     }
     for (let i = 0; i <= ctrl.verticalBars; i++) {
         let y1 = int(random(width));
         let h = int(random(...ctrl.size.map(x => x * height / 100)));
-        fill(controlColorWithAlpha());
+        setRandomFillColor();
         rect(0, y1, width, h);
     }
 }
