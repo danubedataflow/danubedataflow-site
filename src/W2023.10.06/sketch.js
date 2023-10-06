3eeb87f360d30e5f7532325ca67647ab6a71d475
Marcel Gruenauer <bigghosthead@gmail.com>
2023-10-06 09:59:35 +0200

    Neues Werk: W2023.10.06

---
+++ src/W2023.10.06/sketch.js
@@ -0,0 +1,56 @@
+'use strict';
+
+function setupForm() {
+    makeForm(
+        makeSlider('numTiles', 'Anzahl der Kacheln pro Achse', 1, 5, 3),
+        makeSlider('lineGap', 'Distanz der Anfangspunkte', 5, 100, 50),
+        makeSlider('maxMovement', 'Maximale Bewegung', 5, 20, 10),
+    );
+}
+
+function drawSketch() {
+    background('white');
+    stroke('black');
+    noFill();
+    makeGrid({
+        numTilesX: ctrl.numTiles,
+        numTilesY: ctrl.numTiles,
+        tileCallback: drawTile,
+    });
+}
+
+function drawTile(tile) {
+    scale(0.9);
+    drawWalkers(tile);
+    rotate(90);
+    drawWalkers(tile);
+}
+
+function drawWalkers(tile) {
+    push();
+    translate(...tile.upperLeft);
+    for (let startY = 0; startY <= tile.height; startY += ctrl.lineGap) {
+        let y = startY;
+        fill(color(random(255), random(255), random(255), 50));
+
+        /* Make a shape that borders the left, top and right side; the bottom
+         * is determined by the walker. We use a random semitransparent fill
+         * for each shape so each intersecting shape of adjacent horizontal and
+         * vertical walkers is filled by a color that is related to its
+         * neighbors.
+         */
+        beginShape();
+        vertex(0, 0);
+        for (let x = 0; x <= tile.width; x += ctrl.maxMovement) {
+            vertex(x, y);
+            y = constrain(y + random(-ctrl.maxMovement, ctrl.maxMovement), 0, tile.height);
+        }
+        vertex(tile.width, 0);
+        endShape(CLOSE);
+    }
+
+    // draw a border
+    noFill();
+    rect(0, 0, tile.width, tile.height);
+    pop();
+}
