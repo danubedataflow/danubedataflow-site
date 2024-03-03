578bf071b143aa797110b8b88ce927027ef344a7
Marcel Gruenauer <bigghosthead@gmail.com>
2024-03-03 23:58:16 +0100

    Added E-color-palette

---
+++ src/E-color-palette/sketch.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function setupForm() {
+    makeForm(
+        makeSlider('numTiles', 3, 10, 5),
+    );
+}
+
+function drawSketch() {
+    let size = width / ctrl.numTiles;
+    for (let i = 0; i < ctrl.numTiles; i++) {
+        for (let j = 0; j < ctrl.numTiles; j++) {
+            ctx.fillStyle = `rgb(
+        ${Math.floor(255 - (255 / ctrl.numTiles) * i)}
+        ${Math.floor(255 - (255 / ctrl.numTiles) * j)}
+        0)`;
+            ctx.fillRect(j * size, i * size, size, size);
+        }
+    }
+}
