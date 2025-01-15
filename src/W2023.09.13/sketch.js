90756901bf9e7bd1bff41c561b8649d7870b3a39
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-15 20:33:30 +0100

    converted W2023.09.13 to use CanvasRenderingContext2D directly

--- src/W2023.09.13/sketch.js
+++ src/W2023.09.13/sketch.js
@@ -4,25 +4,25 @@ function setupForm() {
     makeForm(
         makeSlider('numRects', 10, 100, 50),
         makeSlider('strokeWeightRange', 1, 20, [2, 4]),
-        makeSlider('alphaRange', 0, 255, [200, 255]),
+        makeSlider('alphaRange', 0, 100, [80, 100]),
     );
 }
 
 function drawSketch() {
-    background('white');
-    noFill();
-    rectMode(CORNER);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
 
     let tileDim = width / ctrl.numTiles;
 
     for (let i = 0; i <= ctrl.numRects; i++) {
-        let ulX = int(random(width));
-        let ulY = int(random(height));
-        let w = int(random(width - ulX));
-        let h = int(random(height - ulY));
+        let ulX = randomIntUpTo(width);
+        let ulY = randomIntUpTo(height);
+        let w = randomIntUpTo(width - ulX);
+        let h = randomIntUpTo(height - ulY);
 
-        strokeWeight(randomIntRange(...ctrl.strokeWeightRange));
-        stroke(0, randomIntRange(...ctrl.alphaRange));
-        rect(ulX, ulY, w, h);
+        ctx.lineWidth = randomIntRange(...ctrl.strokeWeightRange);
+        let alpha = randomIntRange(...ctrl.alphaRange) / 100;
+        ctx.strokeStyle = `rgba(0,0,0,${alpha})`;
+        ctx.strokeRect(ulX, ulY, w, h);
     }
 }
