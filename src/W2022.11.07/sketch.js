207f000c7d3b4d7a63980087932c95cf2c20e430
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-15 15:46:47 +0100

    converted W2022.11.07 to use CanvasRenderingContext2D directly

--- src/W2022.11.07/sketch.js
+++ src/W2022.11.07/sketch.js
@@ -12,24 +12,38 @@ function setupForm() {
 }
 
 function drawSketch() {
-    strokeWeight(1);
-    stroke('black');
-    noFill();
+    ctx.save();
     padSketch();
-    background('white');
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+
     let dim = width / ctrl.numTiles;
     for (let y = 1; y <= ctrl.numTiles; y++) {
         for (let x = 1; x <= ctrl.numTiles; x++) {
-            push();
-            translate((x - 1) * dim, (y - 1) * dim);
+            ctx.save();
+            ctx.translate((x - 1) * dim, (y - 1) * dim);
 
-            if (ctrl.hasTileBorder) rect(0, 0, dim, dim);
-            if (random(100) < ctrl.horizontalLineChance) line(0, dim / 2, dim, dim / 2);
-            if (random(100) < ctrl.verticalLineChance) line(dim / 2, 0, dim / 2, dim);
-            if (random(100) < ctrl.diagonalUpwardsLineChance) line(0, dim, dim, 0);
-            if (random(100) < ctrl.diagonalDownwardsLineChance) line(0, 0, dim, dim);
+            if (ctrl.hasTileBorder) ctx.strokeRect(0, 0, dim, dim);
+            if (randomIntUpTo(100) < ctrl.horizontalLineChance) drawLine(0, dim / 2, dim, dim / 2);
+            if (randomIntUpTo(100) < ctrl.verticalLineChance) drawLine(dim / 2, 0, dim / 2, dim);
+            if (randomIntUpTo(100) < ctrl.diagonalUpwardsLineChance) drawLine(0, dim, dim, 0);
+            if (randomIntUpTo(100) < ctrl.diagonalDownwardsLineChance) drawLine(0, 0, dim, dim);
 
-            pop();
+            ctx.restore();
         }
     }
+    ctx.restore();
 }
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+
+}
+
