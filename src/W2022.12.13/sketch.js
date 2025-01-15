9e3b6c05098af482f1586728bdad4af6fb2bc4fb
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-15 19:30:59 +0100

    converted W2022.12.13 to use CanvasRenderingContext2D directly

--- src/W2022.12.13/sketch.js
+++ src/W2022.12.13/sketch.js
@@ -5,8 +5,8 @@ function setupForm() {
         makeFieldset('colors',
             makeSelectColorMap(),
             makeSlider('numColors', 2, 16, 8),
-            makeSelectBlendMode([BLEND, DIFFERENCE, HARD_LIGHT]),
-            makeSlider('alphaRange', 30, 90, [60, 70]),
+            makeSelectBlendMode(['source-over', 'difference', 'hard-light']),
+            makeSlider('alphaRange', 10, 30, [20, 25]),
         ),
         makeSlider('numGrids', 2, 10, 4),
         makeSlider('segmentDivisorRange', 2, 20, [3, 10]),
@@ -15,62 +15,75 @@ function setupForm() {
 }
 
 function drawSketch() {
-    stroke('black');
-    blendMode(BLEND); // so background() actually clears the canvas
-    background('white');
-    blendMode(ctrl.blendMode);
-    strokeWeight(ctrl.strokeWeight);
+    ctx.save();
+
+    // actually clear the canvas
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.globalCompositeOperation = ctrl.blendMode;
+
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = ctrl.strokeWeight;
+
     let palette = chroma.scale(ctrl.colorMap).colors(ctrl.numColors);
     for (let i = 0; i < ctrl.numGrids; i++) {
-        let drawType = random(['plain', 'diagonal']);
+        let drawType = ['plain', 'diagonal'].randomElement();
         let maxDivisor = randomIntRange(...ctrl.segmentDivisorRange);
-        let minDivisor = int(maxDivisor * 1.5);
-        drawGrid(width, minDivisor, maxDivisor, drawType, palette, randomIntRange(...ctrl.alphaRange));
+        let minDivisor = Math.round(maxDivisor * 1.5);
+        drawGrid(width, minDivisor, maxDivisor, drawType, palette, randomIntRange(...ctrl.alphaRange) / 100);
     }
+    ctx.restore();
 }
 
 function drawGrid(dim, minDivisor, maxDivisor, drawType, palette, alpha) {
     let vsegments = [];
-    let minS = int(dim / minDivisor),
-        maxS = int(dim / maxDivisor);
+    let minS = Math.round(dim / minDivisor),
+        maxS = Math.round(dim / maxDivisor);
+
     // Note: the arguments to random() depend on the canvas width(), so when
     // you resize it you will get a different image
-    for (let y = 0; y < dim - (minS + maxS) / 2; y += int(random(minS, maxS))) {
+    for (let y = 0; y < dim - (minS + maxS) / 2; y += randomIntRange(minS, maxS)) {
         vsegments.push(y);
     }
     vsegments.push(dim);
     vsegments.pairwise((vcurrent, vnext) => {
         let hsegments = [];
-        for (let x = 0; x < dim - (minS + maxS) / 2; x += int(random(minS, maxS))) {
+        for (let x = 0; x < dim - (minS + maxS) / 2; x += randomIntRange(minS, maxS)) {
             hsegments.push(x);
         }
         hsegments.push(dim);
         hsegments.pairwise((hcurrent, hnext) => {
             if (drawType == 'plain') {
-                setFill(random(palette), alpha);
-                rect(hcurrent, vcurrent, hnext, vnext);
-            } else if ((drawType = 'diagonal')) {
+                let [r, g, b] = chroma(palette.randomElement()).rgb();
+                ctx.fillStyle = `rgba(${r},${g},${b},${alpha})`;
+                ctx.fillRect(hcurrent, vcurrent, hnext - hcurrent, vnext - vcurrent);
+                ctx.strokeRect(hcurrent, vcurrent, hnext - hcurrent, vnext - vcurrent);
+            } else if ((drawType == 'diagonal')) {
                 let c1, c2;
-                c1 = random(palette);
+                c1 = palette.randomElement();
                 do {
-                    c2 = random(palette);
+                    c2 = palette.randomElement();
                 } while (c1 == c2);
-                setFill(c1, alpha);
-                rect(hcurrent, vcurrent, hnext, vnext);
-                setFill(c2, alpha);
+                let [r, g, b] = chroma(c1).rgb();
+                ctx.fillStyle = `rgba(${r},${g},${b},${alpha})`;
+
+                ctx.fillRect(hcurrent, vcurrent, hnext - hcurrent, vnext - vcurrent);
+                ctx.strokeRect(hcurrent, vcurrent, hnext - hcurrent, vnext - vcurrent);
+                [r, g, b] = chroma(c2).rgb();
+                ctx.fillStyle = `rgba(${r},${g},${b},${alpha})`;
 
                 if (random() < 0.5) {
-                    triangle(hcurrent, vcurrent, hnext, vcurrent, hnext, vnext);
+                    triangle([ hcurrent, vcurrent ], [ hnext, vcurrent ], [ hnext, vnext ]);
+                    ctx.fill();
+                    ctx.stroke();
                 } else {
-                    triangle(hnext, vcurrent, hnext, vnext, hcurrent, vnext);
+                    triangle([ hnext, vcurrent ], [ hnext, vnext ], [ hcurrent, vnext ]);
+                    ctx.fill();
+                    ctx.stroke();
                 }
             }
         });
     });
-}
-
-function setFill(hex, alpha) {
-    let c = color(hex);
-    c.setAlpha(alpha);
-    fill(c);
 }
