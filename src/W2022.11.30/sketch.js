839cbb115d3aba5c5ef08e6976ec48aaddf6ce2a
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-15 17:15:49 +0100

    converted W2022.11.30 to use CanvasRenderingContext2D directly

--- src/W2022.11.30/sketch.js
+++ src/W2022.11.30/sketch.js
@@ -6,20 +6,20 @@ function setupForm() {
         makeSlider('numRects', 2, 20, 10),
         makeSlider('scale', 0.5, 1, 0.9, 0.1),
         makeSlider('strokeWeightRange', 1, 20, [10, 12]),
-        makeSlider('alphaRange', 0, 255, [200, 255]),
+        makeSlider('alphaRange', 0, 100, [80, 100]),
     );
 }
 
 function drawSketch() {
-    background('white');
-    noFill();
-    rectMode(CORNER);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
 
     // Scale down to create a margin so the outermost strokes won't be clipped.
     let dim = width; // width == height because of square canvas
-    translate(dim / 2, dim / 2);
-    scale(ctrl.scale);
-    translate(-dim / 2, -dim / 2);
+    ctx.save();
+    ctx.translate(dim / 2, dim / 2);
+    ctx.scale(ctrl.scale, ctrl.scale);
+    ctx.translate(-dim / 2, -dim / 2);
 
     let tileDim = width / ctrl.numTiles;
 
@@ -29,8 +29,10 @@ function drawSketch() {
         let spanX = randomIntRange(1, ctrl.numTiles - ulX);
         let spanY = randomIntRange(1, ctrl.numTiles - ulY);
 
-        strokeWeight(randomIntRange(...ctrl.strokeWeightRange));
-        stroke(0, randomIntRange(...ctrl.alphaRange));
-        rect(ulX * tileDim, ulY * tileDim, spanX * tileDim, spanY * tileDim);
+        ctx.lineWidth = (randomIntRange(...ctrl.strokeWeightRange));
+        let alpha = randomIntRange(...ctrl.alphaRange) / 100;
+        ctx.strokeStyle = `rgba(0,0,0,${alpha})`;
+        ctx.strokeRect(ulX * tileDim, ulY * tileDim, spanX * tileDim, spanY * tileDim);
     }
+    ctx.restore();
 }
