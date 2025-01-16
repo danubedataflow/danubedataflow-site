94930cd35944cb916b3d0d8bd2997f3a7f755533
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-16 10:36:53 +0100

    js/math.js: Array.prototype.shuffle() now uses our random(), not Math.random(), for reproducability

--- src/js/math.js
+++ src/js/math.js
@@ -104,7 +104,7 @@ function randomIntPlusMinus(n) {
 Array.prototype.shuffle = function() {
     var i = this.length;
     while (i) {
-        var j = Math.floor(Math.random() * i);
+        var j = Math.floor(random() * i);
         var t = this[--i];
         this[i] = this[j];
         this[j] = t;
