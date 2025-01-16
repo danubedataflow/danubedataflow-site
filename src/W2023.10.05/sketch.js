b38cd93861796c6bf42f99e91e93968b6039867e
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-16 09:32:17 +0100

    converted W2023.10.05 to use CanvasRenderingContext2D directly

--- src/W2023.10.05/sketch.js
+++ src/W2023.10.05/sketch.js
@@ -9,16 +9,12 @@ function setupForm() {
 }
 
 function drawSketch() {
-    colorMode(HSB, 360, 100, 100);
-
     // random color, saturation and brightness
-    background(color(
-        randomIntRange(0, 350),
-        randomIntRange(50, 100),
-        randomIntRange(50, 100)
-    ));
-
-    noStroke();
+    let h = randomIntRange(0, 350);
+    let s = randomIntRange(50, 100);
+    let l = randomIntRange(50, 100);
+    ctx.fillStyle = `hsl(${h}, ${s}%, ${l}%)`;
+    ctx.fillRect(0, 0, width, height);
 
     makeGrid({
         numTilesX: ctrl.numTiles,
@@ -28,37 +24,44 @@ function drawSketch() {
 }
 
 function drawTile(tile) {
+    ctx.save();
 
     // Tile background: scale down to leave space between tiles
-    scale(0.8);
-    fill('white');
-    rect(...tile.upperLeft, ...tile.lowerRight);
+    ctx.scale(0.8, 0.8);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(...tile.upperLeft, tile.width, tile.height);
 
     // Draw the tile. Scale down to leave a border on each tile.
-    scale(0.8);
-    rotate(90 * int(random(4)));
+    ctx.scale(0.8, 0.8);
+    ctx.rotate(randomIntUpTo(4) * Math.PI / 2);
 
     // The following code assumes that (0, 0) is in the tile's upper left
-    translate(...tile.upperLeft);
+    ctx.translate(...tile.upperLeft);
 
     // Get a random exponent for each tile
-    let exponent = random(...ctrl.exponentsRange);
+    let exponent = randomFloatRange(...ctrl.exponentsRange);
     let dim = tile.width / ctrl.numPoints;
     for (let y = 1; y <= ctrl.numPoints; y++) {
         for (let x = 1; x <= ctrl.numPoints; x++) {
-            push();
-            translate((x - 1) * dim, (y - 1) * dim);
+            ctx.save();
+            ctx.translate((x - 1) * dim, (y - 1) * dim);
 
             // 0 < yPercent < 1, so the higher the exponent the more
             // likely a rectangle will be drawn.
-            let yPercent = map(y, 1, ctrl.numPoints, 0, 1);
-            scale(0.9); // to have space between the squares
-            if (random() > pow(yPercent, exponent)) {
-                fill('black');
-                rect(0, 0, dim, dim);
+            let yPercent = (y - 1) / ctrl.numPoints;
+            ctx.scale(0.9, 0.9); // to have space between the squares
+            if (random() > Math.pow(yPercent, exponent)) {
+                ctx.fillStyle = 'black';
+                ctx.fillRect(0, 0, dim, dim);
             }
 
-            pop();
+            ctx.restore();
         }
     }
+    ctx.restore();
 }
+
+function randomFloatRange(lowerBound, upperBound) {
+    return lowerBound + random() * (upperBound + 1 - lowerBound);
+}
+
