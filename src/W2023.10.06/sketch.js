8790ddece114e1a033bc2c0e743134c7c9fb2e93
Marcel Gruenauer <bigghosthead@gmail.com>
2025-01-16 09:43:27 +0100

    converted W2023.10.06 to use CanvasRenderingContext2D directly

--- src/W2023.10.06/sketch.js
+++ src/W2023.10.06/sketch.js
@@ -9,29 +9,36 @@ function setupForm() {
 }
 
 function drawSketch() {
-    background('white');
-    stroke('black');
-    noFill();
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
+    ctx.strokeStyle = 'black';
+
     makeGrid({
         numTilesX: ctrl.numTiles,
         numTilesY: ctrl.numTiles,
         tileCallback: drawTile,
     });
+    ctx.restore();
 }
 
 function drawTile(tile) {
-    scale(0.9);
+    ctx.scale(0.9, 0.9);
     drawWalkers(tile);
-    rotate(90);
+    ctx.rotate(Math.PI / 2);
     drawWalkers(tile);
 }
 
 function drawWalkers(tile) {
-    push();
-    translate(...tile.upperLeft);
+    ctx.save();
+    ctx.translate(...tile.upperLeft);
     for (let startY = 0; startY <= tile.height; startY += ctrl.lineGap) {
         let y = startY;
-        fill(color(random(255), random(255), random(255), 50));
+
+        let r = randomIntUpTo(255);
+        let g = randomIntUpTo(255);
+        let b = randomIntUpTo(255);
+        ctx.fillStyle = `rgba(${r},${g},${b},0.2)`;
 
         /* Make a shape that borders the left, top and right side; the bottom
          * is determined by the walker. We use a random semitransparent fill
@@ -39,18 +46,23 @@ function drawWalkers(tile) {
          * vertical walkers is filled by a color that is related to its
          * neighbors.
          */
-        beginShape();
-        vertex(0, 0);
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
         for (let x = 0; x <= tile.width; x += ctrl.maxMovement) {
-            vertex(x, y);
-            y = constrain(y + random(-ctrl.maxMovement, ctrl.maxMovement), 0, tile.height);
+            ctx.lineTo(x, y);
+
+            // random movement but constrain to the tile size
+            y += randomIntPlusMinus(ctrl.maxMovement);
+            if (y < 0) y = 0;
+            if (y > tile.height) y = tile.height;
         }
-        vertex(tile.width, 0);
-        endShape(CLOSE);
+        ctx.lineTo(tile.width, 0);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 
     // draw a border
-    noFill();
-    rect(0, 0, tile.width, tile.height);
-    pop();
+    ctx.strokeRect(0, 0, tile.width, tile.height);
+    ctx.restore();
 }
