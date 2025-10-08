bb995e95114c5cd1964a1760ca442f46c8cf46ce
Marcel Gruenauer <bigghosthead@gmail.com>
2025-10-08 14:23:04 +0200

    improved E2025.10.08

--- src/E2025.10.08/sketch.js
+++ src/E2025.10.08/sketch.js
@@ -3,7 +3,7 @@
 function setupControls() {
     makeForm(
         makeSlider('numTiles', 10, 100, 5),
-        makeSlider('numSymbols', 2, 20, 1),
+        makeSlider('numSymbols', 2, 20, 9),
         makeSlider('borderChangeChance', 0, 100, 70),
         makeSlider('middleChangeChance', 0, 100, 50),
     );
