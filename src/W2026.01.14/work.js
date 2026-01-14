17089d9c552cf64d360a33ab7f2c2280541f7915
Marcel Gruenauer <bigghosthead@gmail.com>
2026-01-14 11:47:16 +0100

    added W2026.01.14, a homage to "Gaussian-Quadratic" by Michael Noll, 1963

---
+++ src/W2026.01.14/work.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function setupControls() {
+    makeForm(
+        makeSlider('percentNumPoints', 'Number of points: {0}% of width', 5, 15, 10),
+        makeSlider('percentMean', 'Gaussian mean: {0}% of width', 30, 70, 50),
+        makeSlider('percentStandardDeviation', 'Standard deviation: {0}% of width', 5, 25, 15),
+    );
+}
+
+function drawWork() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
+    ctx.strokeStyle = 'black';
+
+    let points = [];
+    let numPoints = width * ctrl.percentNumPoints / 100;
+    let mean = width * ctrl.percentMean / 100;
+    let standardDeviation = width * ctrl.percentStandardDeviation / 100;
+
+    for (let i = 1; i <= numPoints; i++) {
+        let x = Math.floor(gaussianRandom(mean, standardDeviation));
+        x = Math.min(Math.max(x, 0), width); // keep inside the canvas
+
+        // subtract from height so it goes from bottom to top
+        let y = height - ((Math.pow(i, 2) + 5 * i) % height);
+
+        points.push([x, y]);
+    }
+    points.pairwise((current, next) => {
+        ctx.beginPath();
+        ctx.moveTo(...current);
+        ctx.lineTo(...next);
+        ctx.stroke();
+    });
+}
+
+// Source - https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+
+// Standard Normal variate using Box-Muller transform.
+function gaussianRandom(mean = 0, standardDeviation = 1) {
+    const u = 1 - random(); // convert [0,1) to (0,1]
+    const v = random();
+    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    return z * standardDeviation + mean;
+}
