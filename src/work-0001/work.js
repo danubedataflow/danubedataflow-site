4f21d8b71eaafb72a56b97b7db597f66d0b5710a
Marcel Gruenauer <bigghosthead@gmail.com>
2026-01-15 10:59:23 +0100

    Work 0001: generalized to polygons

--- src/work-0001/work.js
+++ src/work-0001/work.js
@@ -5,9 +5,10 @@ let createdDate = '2022.07.27';
 function setupControls() {
 
     makeForm(
-        makeFieldset('Tiles',
-            makeSlider('numTiles', 'Number of tiles per axis: {0}', 1, 50, 25),
-            makeSlider('tileSizeMultiplier', 'Tile size multiplier: {0}', 1, 10, 3, 0.1),
+        makeSlider('numTiles', 'Number of tiles per axis: {0}', 1, 50, 25),
+        makeFieldset('Polygon',
+            makeSlider('numSides', 'Number of sides: {0}', 3, 8, 6),
+            makeSlider('polygonScaleFactor', 'Scale factor: {0}', 1, 10, 3, 0.1),
         ),
         makeFieldset('Noise',
             makeSlider('noiseOffsetX', 'Horizontal noise offset: {0}', 0, 30, 4),
@@ -23,15 +24,14 @@ function drawWork() {
 
     ctx.strokeStyle = 'white';
 
-    let tileWidth = Math.floor(width / ctrl.numTiles);
-    let tileHeight = Math.floor(height / ctrl.numTiles);
+    let tileDim = Math.floor(width / ctrl.numTiles);
 
     for (let tileX = 0; tileX < ctrl.numTiles; tileX++) {
         for (let tileY = 0; tileY < ctrl.numTiles; tileY++) {
             ctx.save();
 
-            let centerX = (tileX + 0.5) * tileWidth;
-            let centerY = (tileY + 0.5) * tileHeight;
+            let centerX = (tileX + 0.5) * tileDim;
+            let centerY = (tileY + 0.5) * tileDim;
             ctx.translate(centerX, centerY);
 
             let n = noise.simplex2(
@@ -39,10 +39,10 @@ function drawWork() {
                 ctrl.noiseOffsetY + tileY / ctrl.noiseDivisor
             );
 
-            let diameter = Math.floor(n * ctrl.tileSizeMultiplier * Math.min(tileWidth, tileHeight));
+            let diameter = Math.floor(n * ctrl.polygonScaleFactor * tileDim);
 
             ctx.beginPath();
-            let points = getPointsForPolygon(6, diameter, 30);
+            let points = getPointsForPolygon(ctrl.numSides, diameter, 0);
             points.forEach(p => ctx.lineTo(...p));
             ctx.closePath();
             ctx.stroke();
